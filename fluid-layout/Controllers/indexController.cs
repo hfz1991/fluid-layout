a6250f072bdf51a76ce972ec08cb24cbb6159712
Charles <funzho@gmail.com>
2015-08-27 02:50:51 +1000

    Using .NET MVC to generate custom css banner

---
+++ fluid-layout/Controllers/indexController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using fluid_layout.Models;
+
+
+namespace fluid_layout.Controllers
+{
+    public class indexController : Controller
+    {
+        // GET: index
+        public ActionResult Index()
+        {
+            BannerAL access = new BannerAL();
+            BannerLine banner = access.getBanner();
+
+            ViewBag.banner = banner;
+
+            return View();
+        }
+    }
+}
---
+++ fluid-layout/Models/BannerAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace fluid_layout.Models
+{
+    public class BannerAL
+    {
+        public BannerLine getBanner()
+        {
+            BannerLine banner = new BannerLine();
+            banner.firstLine = "SIGN UP FOR DEALS,";
+            banner.secondLine = "LATEST NEWS & MORE";
+            
+            return banner;
+        }
+    }
+}
